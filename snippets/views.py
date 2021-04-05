879c62ca1ec09d3fee1827d3c67ed26b686adba0
ricepotato <sukjun40@naver.com>
2021-04-05 11:55:05 +0900

    api_view

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework.serializers import Serializer
+from rest_framework import status
+from rest_framework.decorators import api_view
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, SnippetModelSerializer
 from django.views.decorators.csrf import csrf_exempt
@@ -10,7 +12,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
-def snippet_list(request):
+@api_view(["GET", "POST"])
+def snippet_list(request, format=None):
 
     if request.method == "GET":
         snippets = Snippet.objects.all()
@@ -22,17 +25,18 @@ def snippet_list(request):
         serializer = SnippetSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @csrf_exempt
-def snippet_detail(request, pk):
+@api_view(["GET", "POST", "DELETE"])
+def snippet_detail(request, pk, format=None):
 
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == "GET":
         serializer = SnippetModelSerializer(snippet)
@@ -44,9 +48,9 @@ def snippet_detail(request, pk):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
         snippet.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
